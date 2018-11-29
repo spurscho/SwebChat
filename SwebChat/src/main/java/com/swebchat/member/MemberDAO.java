209ae22eb8d03a0972365b0fe80c5d698eca9958
RyuJungA <ja_0312@naver.com>
2018-11-29 19:01:21 +0900

    a

--- SwebChat/src/main/java/com/swebchat/member/MemberDAO.java
+++ SwebChat/src/main/java/com/swebchat/member/MemberDAO.java
@@ -1,8 +1,24 @@
 package com.swebchat.member;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository("memberDAO")
 public class MemberDAO {
 
+	@Autowired
+	private SqlSessionTemplate sessionTemplate;
+
+	public void setSessionTemplate(SqlSessionTemplate sessionTemplate) {
+		this.sessionTemplate = sessionTemplate;
+	}
+
+	// 한 개의 데이터 조회
+	public MemberDTO getMemberData(String id) {
+
+		MemberDTO dto = sessionTemplate.selectOne("MemberMapper.selectMember", id);
+		return dto;
+		
+	}
 }
