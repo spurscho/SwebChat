47329fee73d05894a9c539d89af31d525bae9d0f
zzanguchoisk <chltjsrkd@hanmail.net>
2018-12-13 16:54:07 +0900

    csk

--- SwebChat/src/main/java/com/swebchat/member/controller/MemberController.java
+++ SwebChat/src/main/java/com/swebchat/member/controller/MemberController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.swebchat.member.model.dto.MemberDTO;
@@ -72,22 +73,21 @@ public class MemberController {
 	@RequestMapping(value="/login_ok", method =RequestMethod.POST)
 	public ModelAndView login_ok(MemberDTO dto, HttpSession session, ModelAndView mav) {
 		dto = service.loginMember(dto);
-		
+		System.out.println("dto : " + dto);
 		if (dto == null) {
 			mav.setViewName("redirect:/login");
 			return mav;
 		} else {
 			session.setAttribute("id", dto.getId());
 			mav.addObject("dto", dto);
-			mav.setViewName("/member/login_ok");
+			mav.setViewName("redirect:/list");
 			return mav;
 		}
 	}
 
 	@RequestMapping(value="/logout", method=RequestMethod.GET)	
-	public String logout(HttpSession session) {
-		session.removeAttribute("id");
-		session.invalidate();
+	public String logout(SessionStatus status) {
+		status.setComplete();
 		return "redirect:/login";
 	}
 
@@ -140,7 +140,7 @@ public class MemberController {
 	}
 
 	@RequestMapping("/list")
-	public String getList(HttpSession session,MemberDTO dto,HttpServletRequest req, HttpServletResponse res) throws Exception{
+	public String getList(HttpSession session, MemberDTO dto,HttpServletRequest req, HttpServletResponse res) throws Exception{
 		MemberDTO result = service.loginMember(dto);
 		
 		List<MemberDTO> lists = service.getMemberList();
@@ -158,6 +158,7 @@ public class MemberController {
 		}
 		
 		req.setAttribute("lists", list);
+		req.setAttribute("myId", myName);
 		return "member/memberList";
 	}
 	
